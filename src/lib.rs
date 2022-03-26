6bc78469e05a34bda1093941ac0820ac6aa147f7
Ju Jun Ryoung <junryoungju@gmail.com>
2022-03-26 13:13:05 +0900

    Formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,5 @@ pub use proc_macro::rename_future;
 
 pub use elain::Align;
 
-
 pub struct PhantomUnsend(std::marker::PhantomData<*const std::ffi::c_void>);
-unsafe impl Sync for PhantomUnsend { }
+unsafe impl Sync for PhantomUnsend {}
